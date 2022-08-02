a6b8a4b06fa50e68a210ede0a54ba33b2622ab3a
78n <108846385+78n@users.noreply.github.com>
2022-08-02 12:32:01 -0600

    Create InstanceSpy.lua

---
+++ InstanceSpy.lua
@@ -0,0 +1,13 @@
+local old
+old = hookfunction(Instance.new,function(instance,parent,...)
+    if checkcaller() then
+        rconsoleprint("@@LIGHT_GREEN@@")
+        rconsoleprint(("New Instance: %s\n"):format(instance))
+        if parent then
+            rconsoleprint("@@YELLOW@@")
+            rconsoleprint(("Instance Parent: %s\n"):format(tostring(parent)))
+        end
+        return old(instance,parent,...)
+    end
+    return old(instance,parent,...)
+end)
