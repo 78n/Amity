7e9dfb11188ebe46e0b69b288c1a775f2fa3b764
78n <108846385+78n@users.noreply.github.com>
2022-08-15 19:45:03 -0600

    Create Washiez.lua

---
+++ Washiez.lua
@@ -0,0 +1,36 @@
+local Players = game:GetService("Players")
+local rs = game:GetService("RunService")
+local lp = Players.LocalPlayer
+local backpack = lp:FindFirstChildWhichIsA("Backpack")
+
+local function path()
+    for i,v in next, game:GetService("Workspace"):GetDescendants() do
+        if v:IsA("ProximityPrompt") and v.Parent:IsA("MeshPart") then
+            return v.Parent
+        end
+    end
+end
+
+local part = path()
+local proximityprompt = part:FindFirstChildWhichIsA("ProximityPrompt")
+local oldpos = lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame
+
+repeat
+    lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame = part.CFrame
+    rs.Heartbeat:Wait()
+    fireproximityprompt(proximityprompt)
+until #backpack:GetChildren() >= 300
+
+lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame = oldpos
+rs.Heartbeat:Wait()
+
+for i,v in next, backpack:GetChildren() do
+    if v:IsA("Tool") then
+        v.Parent = lp.Character
+    end
+end
+
+lp.Character:FindFirstChild("RightHand"):Destroy()
+
+
+
