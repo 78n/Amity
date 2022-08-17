6eea548aa1d71b4b7d0aa0e2669985dcb8fb34af
78n <108846385+78n@users.noreply.github.com>
2022-08-16 20:02:02 -0600

    Update Washiez.lua

--- Washiez.lua
+++ Washiez.lua
@@ -1,11 +1,28 @@
 local Players = game:GetService("Players")
-local rs = game:GetService("RunService")
+local heartbeat = game:GetService("RunService").Heartbeat
 local lp = Players.LocalPlayer
 local backpack = lp:FindFirstChildWhichIsA("Backpack")
 
-local function path()
+if tostring(game.PlaceId):match(6884042552) then
+    local app = game:GetService("ReplicatedStorage").Application.InvokeServer
+    
+    local ans = {
+        "Answer4",
+        "Answer2",
+        "Answer1",
+        "Answer4",
+        "Answer2"
+    }
+    
+    for i = 0,#ans do
+        app({ans[i],i})
+    end
+    return
+end
+
+local path = function()
     for i,v in next, game:GetService("Workspace"):GetDescendants() do
-        if v:IsA("ProximityPrompt") and v.Parent:IsA("MeshPart") and v.ObjectText:match("Ice Cream Cone") then
+        if v:IsA("ProximityPrompt") and v.ObjectText:match("Ice Cream Cone") then
             return v.Parent
         end
     end
@@ -17,13 +34,13 @@ local oldpos = lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame
 
 repeat
     lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame = part.CFrame
-    rs.Heartbeat:Wait()
+    heartbeat:Wait()
     fireproximityprompt(proximityprompt)
 until #backpack:GetChildren() >= 300
 
 lp.Character:FindFirstChild("RightHand"):Destroy()
 lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame = oldpos
-rs.Heartbeat:Wait()
+heartbeat:Wait()
 
 for i,v in next, backpack:GetChildren() do
     if v:IsA("Tool") then
