4cabda332a1a5dcc7a39bc009345bfbca83aeef8
78n <108846385+78n@users.noreply.github.com>
2022-12-10 13:57:57 -0600

    Update RBBattlesHub.lua

--- RBBattlesHub.lua
+++ RBBattlesHub.lua
@@ -1,5 +1,6 @@
 local PlaceId = game.PlaceId
 local games = {
+    [11702621548] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesPiggy.lua")),
     [6447798030] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesFNF.lua"))
 }
 
