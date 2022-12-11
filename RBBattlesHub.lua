65b88a208b735256c9131411b7b2d58ec3b80856
78n <108846385+78n@users.noreply.github.com>
2022-12-10 19:59:15 -0600

    Update RBBattlesHub.lua

--- RBBattlesHub.lua
+++ RBBattlesHub.lua
@@ -1,7 +1,8 @@
 local PlaceId = game.PlaceId
 local games = {
     [11702621548] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesPiggy.lua")),
-    [6447798030] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesFNF.lua"))
+    [6447798030] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesFNF.lua")),
+    [11775705000] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesShoppingWars.lua"))
 }
 
 if games[PlaceId] then
