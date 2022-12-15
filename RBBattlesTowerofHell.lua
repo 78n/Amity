a3d79e1a7a33bef107582aa5d2043f1fe09c0ea3
78n <108846385+78n@users.noreply.github.com>
2022-12-14 20:45:12 -0600

    Update RBBattlesTowerofHell.lua

--- RBBattlesTowerofHell.lua
+++ RBBattlesTowerofHell.lua
@@ -6,9 +6,15 @@ for i,v in next, sections:GetChildren() do
     if v:IsA("Model") and v.Name:find("eventItem") and v:WaitForChild("haloPedastal") and v:FindFirstChild("haloPedastal"):FindFirstChild("haloPart") then
         local haloPart = v:FindFirstChild("haloPedastal"):FindFirstChild("haloPart")
         repeat task.wait()
+            print("looping")
             firetouchinterest(lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart,haloPart,0)
         until haloPart.Transparency == 1
     end
 end
 
-lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame = sections:WaitForChild("finishEvent"):WaitForChild("door"):WaitForChild("doorActivate").CFrame * CFrame.new(0,50,0)
+repeat
+    if lp.Character and lp.Character:FindFirstChildWhichIsA("Humanoid") and lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart then
+        lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart.CFrame = sections:WaitForChild("finishEvent"):WaitForChild("door"):WaitForChild("doorActivate").CFrame * CFrame.new(0,50,0)
+    end
+    task.spawn()
+until not game
