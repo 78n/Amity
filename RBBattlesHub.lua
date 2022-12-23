557b370630caf9a2f3a46065d77f3ad5ff081328
78n <108846385+78n@users.noreply.github.com>
2022-12-23 17:54:06 -0600

    Update RBBattlesHub.lua

--- RBBattlesHub.lua
+++ RBBattlesHub.lua
@@ -1,18 +1,19 @@
 --If you got this script from robloxscripts.com use v3rmillion instead they did not get permission to post it there
 local PlaceId = game.PlaceId
 local games = {
-    [11702621548] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesPiggy.lua")),
-    [6447798030] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesFNF.lua")),
-    [11775705000] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesShoppingWars.lua")),
-    [11704734733] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesWackyWizards.lua")),
-    [7227293156] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesTowerofHell.lua")),
-    [11828780714] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesArsenal.lua")),
-    [9049840490] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesSonicSpeedSimulator.lua")),
-    [920587237] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesAdoptMe.lua")),
-    [537413528] = loadstring(game:HttpGet("https://raw.githubusercontent.com/78n/Amity/main/RBBattlesBuildABoatForTreasure.lua"))
+    [11702621548] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesPiggy.lua",
+    [6447798030] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesFNF.lua",
+    [11775705000] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesShoppingWars.lua",
+    [11704734733] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesWackyWizards.lua",
+    [7227293156] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesTowerofHell.lua",
+    [11828780714] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesArsenal.lua",
+    [9049840490] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesSonicSpeedSimulator.lua",
+    [920587237] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesAdoptMe.lua",
+    [537413528] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesBuildABoatForTreasure.lua",
+    [606849621] = "https://raw.githubusercontent.com/78n/Amity/main/RBBattlesJailBreak.lua"
 }
 
 if games[PlaceId] then
-    return games[PlaceId]()
+    return loadstring(game:HttpGet(games[PlaceId]))()
 end
 warn(("Game (%s) not supported yet"):format(PlaceId))
