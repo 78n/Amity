a643fe149c4538ff8c2885207f06283539d19f4d
78n <108846385+78n@users.noreply.github.com>
2022-12-23 17:51:22 -0600

    Create RBBattlesJailBreak.lua

---
+++ RBBattlesJailBreak.lua
@@ -0,0 +1,1 @@
+game:GetService("ReplicatedStorage"):WaitForChild("RBBattlesTeleportRemote"):FireServer(workspace:WaitForChild("RBSkyscraper"):WaitForChild("Teleporters"):WaitForChild("Top"):WaitForChild("TPP"))
