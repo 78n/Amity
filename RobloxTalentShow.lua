d32f684396200b0fe77f2dff115c0b3923ffa36c
78n <108846385+78n@users.noreply.github.com>
2022-12-26 15:26:46 -0600

    Create RobloxTalentShow.lua

---
+++ RobloxTalentShow.lua
@@ -0,0 +1,63 @@
+if syn and syn.request then request = syn.request end
+local TeleportService = game:GetService("TeleportService")
+local http = game:GetService("HttpService")
+local rs = game:FindService("RunService")
+local lp = game:FindService("Players").LocalPlayer
+local PlaceId = game.PlaceId
+local cursor = ''
+local servers = {}
+
+local ObstacleCourses = workspace:WaitForChild("ObstacleCourses")
+local Teleports = ObstacleCourses:WaitForChild("Teleports")
+
+local function GetRoot()
+    repeat task.wait() until lp.Character and lp.Character:FindFirstChildWhichIsA("Humanoid") and lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart
+    return lp.Character:FindFirstChildWhichIsA("Humanoid").RootPart
+end
+
+local function jsond(str)
+    return http:JSONDecode(str)
+end
+
+local function collectrep(BasePart)
+    local Teleport = tostring(BasePart:WaitForChild("Teleport").Value)
+    local TeleportBasePart = Teleports:WaitForChild(Teleport)
+    local debounce = 0
+
+    repeat
+        GetRoot().CFrame = BasePart.CFrame --* CFrame.new(5,0,0)
+        rs.Heartbeat:Wait()
+        debounce += 1
+    until debounce > 60 and lp:DistanceFromCharacter(TeleportBasePart.Position) >= 1
+end
+
+for i,v in next, ObstacleCourses:GetChildren() do
+    if v:IsA("Model") and v.Name:find("ObstacleCourse") then
+        collectrep(v:FindFirstChildWhichIsA("BasePart"))
+    end
+end
+    
+while cursor and #servers == 0 do
+    local req = request({Url = ('https://games.roblox.com/v1/games/%s/servers/Public?sortOrder=Asc&limit=100&cursor=%s'):format(PlaceId,cursor)})
+    local body = jsond(req.Body)
+    
+    if body and body.data then
+        coroutine.wrap(function()
+            for i,v in next, body.data do
+                if type(v) == 'table' and tonumber(v.playing) and tonumber(v.maxPlayers) and v.playing < v.maxPlayers then
+                    table.insert(servers, 1, v.id)
+                end 
+            end
+        end)()
+    
+        if body.nextPageCursor then
+            cursor = body.nextPageCursor
+        end
+    end
+    task.wait()
+end
+    
+while #servers > 0 do
+    TeleportService:TeleportToPlaceInstance(PlaceId, servers[math.random(1, #servers)], lp)
+    task.wait(1)
+end
