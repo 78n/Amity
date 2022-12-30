60d37c4b1f2bf0c8e096541537bf216951b72ec0
78n <108846385+78n@users.noreply.github.com>
2022-12-30 12:07:15 -0600

    Create RBBattlesMountEverest.lua

---
+++ RBBattlesMountEverest.lua
@@ -0,0 +1,15 @@
+game:GetService("ReplicatedStorage"):WaitForChild("VitalityBridge"):FireServer("dmg",{dmg=0/0,r="falling"})
+lp.Character:FindFirstChildWhichIsA("Humanoid").WalkSpeed = 50
+lp.Character:FindFirstChildWhichIsA("Humanoid").JumpHeight = 15
+lp.Character:FindFirstChildWhichIsA("Humanoid"):GetPropertyChangedSignal("WalkSpeed"):Connect(function()
+    lp.Character:FindFirstChildWhichIsA("Humanoid").WalkSpeed = 50
+end)
+lp.Character:FindFirstChildWhichIsA("Humanoid"):GetPropertyChangedSignal("JumpHeight"):Connect(function()
+    lp.Character:FindFirstChildWhichIsA("Humanoid").JumpHeight = 15
+end)
+
+lp:GetMouse().KeyDown:Connect(function(Key)
+    if Key == " " then
+        lp.Character:FindFirstChildWhichIsA('Humanoid'):ChangeState(3)
+    end
+end)
