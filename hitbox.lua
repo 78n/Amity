b0af9c5bc921def9fd7bee3b78b09c7e25b10e42
78n <108846385+78n@users.noreply.github.com>
2023-01-01 21:52:16 -0600

    Update hitbox.lua

--- hitbox.lua
+++ hitbox.lua
@@ -1,41 +1,45 @@
-local Players = game:GetService("Players")
+getgenv().hitboxsize = 10 --studs
+local Players = game:FindService("Players")
+local rs = game:FindService("RunService")
 local lp = Players.LocalPlayer
-getgenv().size = 30 --size of hitbox in studs
+local connections = {}
 
-local WaitForChildWhichIsA = function(instance, classtype)
-	local target = instance:FindFirstChildWhichIsA(classtype)
-	while not target or not target:IsA(classtype) do
-		target = instance.ChildAdded:Wait()	
-	end
-	return target
+local function hitbox(plr)
+    local plrname = plr.Name
+    local size = getgenv().hitboxsize
+    if not connections[plr] or not connections["SteppedLoop_"..plrname] then
+        connections[plr] = plr.CharacterAdded:Connect(function(char)
+            repeat task.wait() until char:FindFirstChildWhichIsA("Humanoid") and char:FindFirstChildWhichIsA("Humanoid").RootPart
+            hitbox(plr)
+        end)
+        connections["SteppedLoop_"..plrname] = rs.Stepped:Connect(function()
+            if plr.Character then
+                for i,v in next, plr.Character:GetDescendants() do
+                    if v:IsA("BasePart") then
+                        v.CanCollide = false
+                    end
+                end
+            end
+        end)
+    end
+    repeat task.wait() until plr.Character and plr.Character:FindFirstChildWhichIsA("Humanoid") and plr.Character:FindFirstChildWhichIsA("Humanoid").RootPart
+    plr.Character:FindFirstChildWhichIsA("Humanoid").RootPart.Size = Vector3.new(size,size,size)
 end
-
-local hitbox = function(target_char)
-    local humanoid = WaitForChildWhichIsA(target_char,"Humanoid")
-    repeat task.wait() until humanoid.RootPart
-    local hrp = humanoid.RootPart
-    
-    hrp.Size = Vector3.new(size,size,size)
-    hrp.CanCollide = false
-    hrp:GetPropertyChangedSignal('CanCollide'):Connect(function()
-        hrp.CanCollide = false
-    end)
-end
-    
 
 for i,v in next, Players:GetPlayers() do
     if v ~= lp then
-        if v.Character then
-            hitbox(v.Character)
-        end
-        v.CharacterAdded:Connect(function(char)
-            hitbox(char)
-        end)
+        hitbox(v)
     end
 end
 
-Players.PlayerAdded:Connect(function(plr)
-    plr.CharacterAdded:Connect(function(char)
-        hitbox(char)
-    end)
+Players.PlayerAdded:Connect(hitbox)
+
+Players.PlayerRemoving:Connect(function(plr)
+    local plrname = plr.Name
+    if connections[plr] then
+        connections[plr]:Disconnect()
+    end
+    if connections["SteppedLoop_"..plrname] then
+        connections["SteppedLoop_"..plrname]:Disconnect()
+    end
 end)
